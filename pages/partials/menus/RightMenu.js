d5564168a2f04b8a104df5707cabd1746836abef
komailf67 <komail.f67@gmail.com>
2020-06-25 19:44:02 +0430

    separate menus

--- pages/partials/menus/RightMenu.js
+++ pages/partials/menus/RightMenu.js
@@ -1,90 +1,27 @@
 import React, {Component} from "react";
-import {Card, ListGroup, Col} from "react-bootstrap";
-import Router from 'next/router'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+import AdminMenu from "./roleMenu/adminMenu";
+import SellerMenu from "./roleMenu/sellerMenu";
 
 class RightMenu extends Component {
 
-    logout = () => {
-        let token = localStorage.getItem('access_token');
-        let config = {
-            headers: {'Authorization': "bearer " + token}
-        };
-        //TODO
-        axios.get(
-            'http://automation.afra.local/api/logout',
-            config
-        ).then(response => {
-            let {success, message} = response.data;
-            window.location.replace("/");
-        }).catch(error => {
-
-        });
-    }
-
     render() {
-        const onClickHandler = (href) => {
-            return e => {
-                e.preventDefault()
-                Router.push(href)
-            }
+        let menu;
+
+        switch (localStorage.getItem('role')) {
+            case 'admin' :
+                menu = <AdminMenu/>
+                break;
+            case 'seller' :
+                menu = <SellerMenu/>
+                break;
+            default :
+                menu = <SellerMenu/>
         }
 
-        const Link = ({children, href}) => (
-            <a href="#" onClick={onClickHandler(href)}>
-                {children}
-                <style jsx>{`
-                a {
-                  margin-right: 10px;
-                }
-              `}</style>
-            </a>
-        )
         return (
-            <Col sm={2} className="text-right">
-                <Card>
-                    <Card.Header>منو</Card.Header>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <Link href="/">Home</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/orders/add">ثبت سفارش</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/orders/orders">لیست سفارشات</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/products/productsList">لیست کالاها</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/preInvoices/preInvoices">پیش فاکتورها</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/storage/products">انبار</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/storage/add">اضافه کردن محصول</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/sales/products">محصولات فروخته شده</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/sales/invoices">فاکتورهای فروش</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Link href="/categories/categories">دسته بندی ها</Link>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <h6
-                                style={{'paddingRight': '10px', 'cursor': 'pointer', 'color': 'red'}}
-                                onClick={this.logout}
-                            >خروج</h6>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card>
-            </Col>
+            <React.Fragment>
+                {menu}
+            </React.Fragment>
         )
     }
 }
---
+++ pages/partials/menus/roleMenu/adminMenu.js
@@ -0,0 +1,91 @@
+import React, {Component} from "react";
+import {Card, ListGroup, Col} from "react-bootstrap";
+import Router from 'next/router'
+import axios from 'axios';
+
+class AdminMenu extends Component {
+
+    logout = () => {
+        let token = localStorage.getItem('access_token');
+        let config = {
+            headers: {'Authorization': "bearer " + token}
+        };
+        //TODO
+        axios.get(
+            'http://automation.afra.local/api/logout',
+            config
+        ).then(response => {
+            let {success, message} = response.data;
+            window.location.replace("/");
+        }).catch(error => {
+
+        });
+    }
+    render() {
+        console.log('dddddd',  localStorage.getItem('role'))
+        const onClickHandler = (href) => {
+            return e => {
+                e.preventDefault()
+                Router.push(href)
+            }
+        }
+
+        const Link = ({children, href}) => (
+            <a href="#" onClick={onClickHandler(href)}>
+                {children}
+                <style jsx>{`
+                a {
+                  margin-right: 10px;
+                }
+              `}</style>
+            </a>
+        )
+        return (
+            <Col sm={2} className="text-right">
+                <Card>
+                    <Card.Header>منو</Card.Header>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <Link href="/">Home</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/orders/add">ثبت سفارش</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/orders/orders">لیست سفارشات</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/products/productsList">لیست کالاها</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/preInvoices/preInvoices">پیش فاکتورها</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/storage/products">انبار</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/storage/add">اضافه کردن محصول</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/sales/products">محصولات فروخته شده</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/sales/invoices">فاکتورهای فروش</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Link href="/categories/categories">دسته بندی ها</Link>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h6
+                                style={{'paddingRight': '10px', 'cursor': 'pointer', 'color': 'red'}}
+                                onClick={this.logout}
+                            >خروج</h6>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        )
+    }
+}
+
+export default AdminMenu;
